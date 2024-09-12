c26ac140bc9b05b3812aefbe678cb4f6706229db
Octavio Castillo <luisoctaviocs02@gmail.com>
2024-09-11 19:26:59 -0600

    a

---
+++ tarea11/ejercicio7.py
@@ -0,0 +1,7 @@
+'''
+Ejercicio 7: Implementa una cola utilizando una lista enlazada. 
+Escribe funciones para simular un centro de atención a clientes de 
+Telcel donde las personas llegan y son atendidas en orden de llegada. 
+El programa debe permitir tomar un turno, indicar el turno que está 
+atendiendo  y el próximo turno
+'''
